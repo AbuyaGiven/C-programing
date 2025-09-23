8ec31e8d43df068da243e1164dc8f49bdeb3cb1e
AbuyaGiven <givenabuya@gmail.com>
2025-09-23 14:49:54 +0300

    Add files via upload

---
+++ Details.c
@@ -0,0 +1,29 @@
+// Assignment 2
+/*
+Name:Given Abuya
+Reg no:PA106/G/28830/25
+Description:Prompt User program 
+
+*/
+#include<stdio.h>
+//main function 
+
+int main ()
+{
+ int height;
+ int Id_number;
+ int bank_balance;
+ int age;
+ 
+// enter details to display 
+ printf("Enter the following details\n");
+ printf("Enter your height in cm:\n");
+ scanf("%d",&height);
+ printf("Enter your Id number:\n");
+ scanf("%d",&Id_number);
+ printf("Enter your bank balance:\n");
+ scanf("%d",&bank_balance);
+ printf("===End,Thank you==:\n");
+ 
+ return 0;
+  }
---
+++ Units.c
@@ -0,0 +1,21 @@
+//Software Engineering units 
+/*
+name:Given Abuya 
+reg no: PA106/G/28830/25
+description:Program to display my year one units 
+*/
+
+#include<stdio.h>
+
+// main function 
+int main(){
+printf("spc 2101 Introduction to Programming\n");
+printf("spi 2102 Computer systems and organizations\n");
+printf("spm 2100 Descrete Mathematics\n");
+printf("spm 2104 Mathematics for science\n");
+printf("sse 2101 Fundamentals of system analysis and design\n");
+printf("ucu 2105 Fundamentals of ict\n");
+printf("ucu 2106 Communication and information literacy\n");
+
+return 0;
+} 
